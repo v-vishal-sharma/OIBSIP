51089b42d59162b0a1405a2ae65374549fc943ec
Vishal Sharma <vishals200415@gmail.com>
2023-05-15 23:42:55 +0530

    ,

--- calculator/app.js
+++ calculator/app.js
@@ -1,13 +1,24 @@
 let isOperatorChosen = false;
-let firstOperand = 0;
-let secondOperand = 0;
+let firstOperand=0;
+let secondOperand=0;
+let grandExpression;
+const screen = document.getElementById("calculations");
+
+function operatorChosen(value){
+    isOperatorChosen = value;
+}
 
 function operand(n){
     console.log("clicked");
     if (isOperatorChosen == false){
         firstOperand = (firstOperand*10) + n;
-        console.log(firstOperand);
     }else{
         secondOperand = (secondOperand*10) + n;
     }
+    display(firstOperand,secondOperand);
+}
+
+function display(firstOperand,secondOperand){
+    grandExpression = firstOperand.toString() + secondOperand.toString();
+    screen.innerHTML = grandExpression;
 }
